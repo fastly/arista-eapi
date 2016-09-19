7096818e2583eafc91f58c5c865c39b6c542432f
Bao Nguyen <bao@fastly.com>
2016-09-19 12:24:01 -0700

    Allow `verify_ssl` as an option
    This is to allow clients to specific the details of ssl verification via
    arista-eapi.
    
    Also minor syntax fixed up using Ruby 1.9's hash syntax.

--- lib/arista/eapi/request.rb
+++ lib/arista/eapi/request.rb
@@ -12,20 +12,27 @@ module Arista
       end
 
       def payload
-        @payload ||= JSON.generate({
-          :jsonrpc => '2.0',
-          :method  => 'runCmds',
-          :id      => 1,
-          :params  => {
-            :version => 1,
-            :cmds    => commands,
-            :format  => options[:format]
-          },
-        })
+        @payload ||= JSON.generate(
+          jsonrpc: '2.0',
+          method: 'runCmds',
+          id: 1,
+          params: {
+            version: 1,
+            cmds: commands,
+            format: options[:format]
+          }
+        )
       end
 
       def execute
-        Arista::EAPI::Response.new(commands, RestClient.post(switch.url, payload))
+        req = RestClient::Request.execute(
+          method: :get,
+          url: switch.url,
+          timeout: 10,
+          verify_ssl: switch.verify_ssl,
+          payload: payload
+        )
+        Arista::EAPI::Response.new(commands, req)
       end
     end
   end
